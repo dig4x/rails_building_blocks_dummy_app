dd6ff37c8889fd0afa4053b2bf2b04e59337da81
Joe Goggins <goggins@umn.edu>
2013-06-26 16:56:31 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBuildingBlocksDummyApp::Application.config.secret_token = 'db42b68ecf48322bb6da873274151a62bf31dfa70febbd647cc5c473450aaab751a89f9c1e0c4dff333ce1655ca2181ce3b1a8b119720a48846a286d5242c2ea'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+RailsBuildingBlocksDummyApp::Application.routes.draw do
+  root :to => 'root#index'
+end
